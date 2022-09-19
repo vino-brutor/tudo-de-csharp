29e68a8ea0c400f6e560e367ba75c3a8816f84d4
Vitor Bruno <viktorbruno2006@gmail.com>
2022-09-19 18:22:34 -0300

    adicionando as coisas

---
+++ media do triangul c#/AreaDoTraingulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace media_do_triangul_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float b;
+            float altura;
+
+            Console.WriteLine("Quanto vale a base?");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("e quanto vale a altura?");
+            altura = float.Parse(Console.ReadLine());
+
+            float area = (b*altura)/2;
+
+            Console.WriteLine("a área desse triangulo é {0}",area);
+            Console.Write("um baita triangulo realmente");
+        }
+    }
+}
---
+++ temperatura c#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace temperatura_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
